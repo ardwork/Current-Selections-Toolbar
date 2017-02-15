b78e73b4631f7f5a441b4542b1dc6df09348eb85
ardwork <rich.byard@gmail.com>
2017-02-15 14:28:16 +0400

    Initial upload
    Initial upload

---
+++ README.md
@@ -0,0 +1,26 @@
+<h2>Qlik Sense Current Selections Object with Action Buttons</h2>
+<br>
+<br>
+A very simple Qlik Sense extension that allows users to show the selections currently set in an application. This has been requested for use cases where exporting / printing a sheet or story to PDF or Powerpoint and the selections are required to be shown. You can show and hide the buttons you require to allow users to manange the selections, to a limited extent, from this object.
+<br>
+<br>
+<br>
+<img src="https://github.com/ardwork/currentSelectionstoolbox/blob/master/img/currentSelections1.png" alt="Example Current Selections object">
+<br>
+<br>
+<img src="https://github.com/ardwork/currentSelectionstoolbox/blob/master/img/currentSelections2.png" alt="Example Current Selections object">
+<br>
+<br>
+<h3>Instructions:</h3>
+There are no instructions, simply drag the 'Current Selections' object onto your sheet and give it a Title as you require.
+<br>
+<br>
+<br>
+<h3>Testing</h3>
+Tested with Qlik Sense 2.2.* and 3.1.* <br>
+Note: At the time of testing extensions do not print to PDF or PowerPoint and appear as invalid object areas. This will hopefully be addressed soon to allow the full fuctionality of export sheets and stories to be available across extensions.<br>
+<br>
+<br>
+<br>
+<h3>Comments</h3>
+On initial opening the 'No Current Selections' message is repeated 3 times. If anyone has a solution for this I'd be interested as I have not been able to resolve it.
---
+++ current-selections-toolbar.js
@@ -0,0 +1,174 @@
+/*global define */
+
+define(["jquery", "text!./current-selections-toolbar.css",
+//mashup and extension interface
+"qlik"], function($, cssContent, qlik ) {
+	$("<style>").html(cssContent).appendTo("head");
+	function createBtn(cmd, text) {
+		return '<button class="qui-button" style="font-size:13px;" data-cmd="' + cmd + '">' + text + '</button>';
+	}
+
+	return {
+		initialProperties : {
+			qBookmarkListDef : {
+				qType : "bookmark",
+				qData : {
+					title : "/title",
+					description : "/description"
+				}
+			},
+			qFieldListDef : {
+			}
+		},		
+		definition : {
+			type : "items",
+			component : "accordion",
+			items : {
+				buttons : {
+					type : "items",
+					label : "App buttons",
+					items : {
+						clearButton : {
+							ref : "buttons.clear",
+							label : "ClearAll",
+							type : "boolean",
+							defaultValue : true
+						},
+						backButton : {
+							ref : "buttons.back",
+							label : "Back",
+							type : "boolean",
+							defaultValue : true
+						},
+						forwardButton : {
+							ref : "buttons.forward",
+							label : "Forward",
+							type : "boolean",
+							defaultValue : true
+						},
+						lockButton : {
+							ref : "buttons.lockall",
+							label : "Lock All",
+							type : "boolean",
+							defaultValue : false
+						},
+						unlockButton : {
+							ref : "buttons.unlockall",
+							label : "Unlock All",
+							type : "boolean",
+							defaultValue : false
+						},
+						ShowSelections : {
+							ref : "selections.show",
+							label : "Show Selected Values",
+							type : "boolean",
+							defaultValue : true
+						}
+					}
+				},
+				settings : {
+					uses : "settings"
+				}
+			}
+		},
+		paint : function($element, layout) {
+			$element.css('overflow', 'auto');
+			$element.addClass( 'qtoolbar' );
+			
+			
+			// Show Buttons
+			var html = '', app = qlik.currApp(this);
+			html += '<div class="qui-buttongroup">';
+			if(layout.buttons.clear) {
+				html += createBtn("clearAll", "Clear");
+			}
+			if(layout.buttons.back) {
+				html += createBtn("back", "Back");
+			}
+			if(layout.buttons.forward) {
+				html += createBtn("forward", "Forward");
+			}
+			if(layout.buttons.lockall) {
+				html += createBtn("lockAll", "Lock");
+			}
+			if(layout.buttons.unlockall) {
+				html += createBtn("unlockAll", "Unlock");
+			}
+			html += '</div>';
+			
+			$element.empty();
+			$element.html(html);
+			
+			
+			// Show Current Selections
+			if(layout.selections.show) {
+			
+			var selections = app.getList("CurrentSelections", function(reply) {
+				var mySelectedFields = reply.qSelectionObject.qSelections,
+					mySelectedFieldsLength = mySelectedFields.length,
+					html2 = '';
+					
+					$element.clear;
+					
+				if(mySelectedFieldsLength > 0) {
+				
+						html2 += '<div class="csheader">Current Selections</div><div><table id="mySelections"><tr><th>Field</th><th>Count</th><th>Values</th></tr>';
+					
+						for (var i = 0; i < mySelectedFieldsLength; i++){
+							html2 += '<tr><td>' + mySelectedFields[i].qField + '</td><td>' + mySelectedFields[i].qSelectedCount + ' of ' + mySelectedFields[i].qTotal + '</td><td>' + mySelectedFields[i].qSelected + '</td></tr>';
+							}
+							html2 += '</table></div>';		
+						
+				}
+				else
+				{ html2 += '<div class="csheader">No Current Selections</div>';
+
+					}
+				
+				$element.clear;
+				$element.append(html2);
+			
+				}
+			);
+			
+			
+			
+			}
+
+
+			
+			// Enable Export
+			var ext = {
+				support: {
+					snapshot: true,
+					export: true,
+					exportData: true
+				}
+			};
+				
+			
+					
+			// Button Actions
+			$element.find('button').on('qv-activate', function() {
+				switch($(this).data('cmd')) {
+					case 'clearAll':
+						app.clearAll();
+						break;
+					case 'back':
+						app.back();
+						break;
+					case 'forward':
+						app.forward();
+						break;
+					case 'lockAll':
+						app.lockAll();
+						break;
+					case 'unlockAll':
+						app.unlockAll();
+						break;
+				}
+			});
+
+		}
+	};
+});
